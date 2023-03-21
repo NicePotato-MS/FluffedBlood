dc8d0bc1d7fce485972188d8c17fab7d9983be17
NicePotato-MS <micahsuessgithub@gmail.com>
2023-03-20 21:01:41 -0500

    put button in the Art/ui/Buttons

--- Scripts/menu.lua
+++ Scripts/menu.lua
@@ -6,24 +6,49 @@ Gamestate = require("Scripts/hump/gamestate")
 
 menu = {}
 
-local dotSize = 0.03
+local delta
+
+local dotSize = 0.02
 local dotProgress = 0
+local literalDotSize = 5
 local comicSans
 local menuTitle
 
 function menu:init()
     comicSans = getFont("ComicSans", "Bold", 80)
-    menuTitle = love.graphics.newText(comicSans, {{1,0,0},"Fluffed Blood"})
+    menuTitle = love.graphics.newText(comicSans, "Fluffed Blood")
 end
 
 function menu:draw()
+    delta = love.timer.getDelta()
+
+    --Background Color
     local bg = rgb(255,229,232)
     love.graphics.setBackgroundColor(bg[1],bg[2],bg[3])
-    love.graphics.draw(menuTitle, round(love.graphics.getWidth()/2)-round(menuTitle:getWidth()/2) ,round(love.graphics.getHeight()/8))
-    local literalDotSize = round(love.graphics.getWidth()*dotSize)
-    if dotProgress == 1 then
-        dotProgress = 0
-    else
-        dotProgress +=
-    love.graphics.circle("fill", 100, 100, literalDotSize)
+
+    literalDotSize = round(love.graphics.getWidth()*dotSize)
+    if literalDotSize < 5 then literalDotSize = 5 end
+    if dotProgress >= 4 then
+        dotProgress = dotProgress-4
+    end
+    dotProgress = dotProgress + 1 * delta
+    for iY=dotProgress*literalDotSize, literalDotSize*2+love.graphics.getHeight(), literalDotSize*4 do
+        love.graphics.setColor(1, 1, 1)
+        drawDotRow(dotProgress*literalDotSize-literalDotSize*4,0-iY+love.graphics.getHeight()+literalDotSize, literalDotSize) 
+    end
+
+    --Title Text
+    love.graphics.setColor(1, 0, 0)
+    love.graphics.draw(menuTitle, round(love.graphics.getWidth()/2)-round(menuTitle:getWidth()/2) ,round(love.graphics.getHeight()/12))
+    
+end
+
+function drawDotRow(x,y,size)
+    for i=x, love.graphics.getWidth()+literalDotSize, literalDotSize*4 do
+        love.graphics.circle("fill", i, y, size)
+    end
+end
+
+function menu:mousepressed()
+    mouseX, mouseY = love.mouse.getPosition()
 end
