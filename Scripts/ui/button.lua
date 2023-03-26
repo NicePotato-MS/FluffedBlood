2aecf47363d782d9a47bc3f4c959ba828c5c5372
NicePotato-MS <micahsuessgithub@gmail.com>
2023-03-26 01:03:37 -0500

    no

--- Scripts/ui/button.lua
+++ Scripts/ui/button.lua
@@ -20,13 +20,42 @@ function PB.createVisual()
     
 end
 
-function PB.createCollider()
+--[[
+Rectangle
+-obj = "rect" or "rectangle"
+-x
+-y
+-arg1 = width
+-arg2 = height
+-arg3 = rotation
+]]--
 
+function PB.createCollider(obj, x, y, arg1, arg2, arg3)
+    if obj == "rect" or obj == "rectangle" then
+        local px = x or 0
+        local py = y or 0
+        local sX = arg1 or 1
+        local sY = arg2 or 1
+        local r = arg3 or 0
+        return {
+            obj = "rect",
+            properties = {
+                x = px,
+                y = py,
+                rotation = r,
+                scaleX = sX,
+                scaleY = sY
+            }
+        }
+    else
+        PB.pbError("createCollider","obj '"..obj.."' is not a known type")
+        return
+    end
 end
 
 function PB.createAnim(propertyTable, time, easeStyle) -- the 'propertyTable' argument comes from PB.createProperties()
     if type(propertyTable) ~= "table" then
-        PB.pbError("createAnim()","passed value propertyTable: '"..propertyTable.."' is not a table")
+        PB.pbError("createAnim","passed value propertyTable: '"..propertyTable.."' is not a table")
         return false
     end
     local dur = time or 1
@@ -50,7 +79,7 @@ end
 
 function PB.clicked(self) -- use in a love.mouseclicked function in an if statement
     if type(self) ~= "table" then
-        PB.pbError("clicked()","passed value self: '"..self.."' is not a table")
+        PB.pbError("clicked","passed value self: '"..self.."' is not a table")
         return false
     end
     if self["colliders"] == nil then
@@ -63,7 +92,7 @@ end
 buttonStructure = {
     colliders = {
         {
-            obj = "rectangle",
+            obj = "rect",
             --[return of PB.createProperties]
         }
     }
